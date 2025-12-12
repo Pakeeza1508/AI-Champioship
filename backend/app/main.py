89e722ed8df85738dd4f041793a2c95a2d549c38
Pakeeza1508 <pakeezaislam1508@gmail.com>
2025-12-12 14:56:18 +0500

    Lets deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,8 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.allowed_origins,
+    # allow_origins=settings.allowed_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
